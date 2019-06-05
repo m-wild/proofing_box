a40d669987c135e684f5176dfb8ceafd9aa78977
Michael Wildman <michael@mwild.me>
2019-06-05 22:44:27 +1200

    initial commit

---
+++ proofing_box.ino
@@ -0,0 +1,17 @@
+// Bread Proofing Box
+// michael@mwild.me
+//
+// Maintains a constant temperature inside a proofing box by turning on/off
+// a heating pad connected to a relay.
+//
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
